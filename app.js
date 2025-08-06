4beeef4080968a93b8d522eb69bb0b5811faf931
AshFigaro <32578508+AshFigaro@users.noreply.github.com>
2025-08-05 22:56:54 -0400

    Actualiza el app.js
    Se crea la función "agregarAmigo" que permite al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos creada anteriormente. 
    
    Adicionalmente se ingresa la siguiente linea de codigo para limpiar el campo una vez ingresado el nombre para poder digitar uno nuevo:
    
    document.getElementById("amigo").value = "";

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigo() {
+let nombre = document.getElementById("amigo").value.trim();
+
+    if (!validarNombre(nombre)) {
+        alert("Por favor, inserte un nombre válido.");
+    } else if (amigos.includes(nombre)) {
+        alert("El nombre ya fue ingresado, favor ingrese uno nuevo");
+    } else {
+        amigos.push(nombre);
+        actualizarLista();
+        document.getElementById("amigo").value = ""; // Limpiar el campo
+    }
+}
