5805c47f7cb2272b867c1e23486945fa38bfe902
AshFigaro <32578508+AshFigaro@users.noreply.github.com>
2025-08-05 23:02:54 -0400

    Actualiza el app.js
    Se añade la funcion "actualizaLista" para que si el valor es valido se añada al arreglo y se almacene sin problemas y que adicionalmente se limpie el campo de entrada luego de añadir un nombre.

--- app.js
+++ app.js
@@ -18,3 +18,13 @@ let nombre = document.getElementById("amigo").value.trim();
 function validarNombre(nombre) {
     return nombre.trim() !== "";
 }
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; //Limpiar la lista actual
+    amigos.forEach(function(amigo) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
